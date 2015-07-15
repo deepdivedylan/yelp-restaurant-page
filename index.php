6e0ffaa92dc0408e71a65d3cc3acbec6482caa14
TrevorRigler <tarigler@gmail.com>
2015-07-14 19:24:54 -0600

    Edited index.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: trevor
- * Date: 7/14/15
- * Time: 6:58 PM
- * commenting so I can push a change
- */
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Yelp Restaurant Page Data Design Project</title>
+	</head>
+	<body>
+		<img src="yelp.jpg" alt="Yelp Logo" height="450" width="800">
+		<p><br>Yelp is a website consisting mainly of reviews of local
+			businesses, with an emphasis on restaurants.</br>
+		<br>Founded in 2004 by two former PayPal employees, Yelp is now
+		a publicly traded company (<a href="https://www.nyse.com/quote/XNYS:YELP">NYSE:YELP</a>) with approximately <b>135 monthly visitors</b>.</br></p>
+	</body>
+</html>
