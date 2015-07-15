43de5c75667580996d7ee1894b6e3a8e1c3b772e
TrevorRigler <tarigler@gmail.com>
2015-07-14 20:42:27 -0600

    still further editing of index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Assumptions: User has an account. Restaurant has a page.</br>
 		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Basic Pattern:</br>
 		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;1. User browses to Yelp page for preferred town.</br>
-		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;2. User types in name of specific restaurant in search field></br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;2. User types in name of specific restaurant in search field.</br>
 		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;3. Server searches internally for a potential match, limited geographically.</br>
 		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;4. Server returns list of possible matches, each a link.</br>
 		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;5. User selects best match offered by server and clicks.</br>
@@ -33,5 +33,22 @@
 		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Goal: Allow user to locate other users based on other website affiliations.</br>
 		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Entities: User(s), Third Party Website(s)(ex:Facebook)</br>
 		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Assumptions: Users have accounts with Yelp <i>and</i> certain third party websites.</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;1. User browses to Yelp page for any town.</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;2. User clicks on Find Friends link near top of page.</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;3. Server offers for user to log in to Facebook account.</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;4. Server accesses user's friend list on external Facebook server.</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;5. Server cross-references existing Yelp users against user's FB friend list.</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;6. User selects from list of FB friends to whom to submit Yelp friend requests.</br>
+		<br>&#160;&#160;&#160;<b>3.)</b> Find map and directions to specific restaurant. (dynamic use case)</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Goal: Allow user to access map and directions from third party website(s).</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Entities: User(s), Restaurant, Third Party Website(s)(ex:Google Maps)</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Assumptions: User has account with Yelp. User permits Yelp to know user's location. Restaurant has page with Yelp. Restaurant is on third party website.</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;1. User browses to Yelp page for desired restaurant.</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;2. User clicks on Get Directions link under restaurant name.</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;3. Server searches third party website for restaurant's location.</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;4. Server provides map with restaurant's location indicated.</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;5. User clicks on Get Directions button inset on map.</br>
+		<br>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;6. Server acquires driving directions from third party website and provides same to user in map form.</br>
+
 	</body>
 </html>
