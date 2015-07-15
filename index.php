72500d3b2bf4642ec68bdaee0e4e07551111efc5
TrevorRigler <tarigler@gmail.com>
2015-07-14 19:49:02 -0600

    further editing of index.php

--- index.php
+++ index.php
@@ -9,5 +9,15 @@
 			businesses, with an emphasis on restaurants.</br>
 		<br>Founded in 2004 by two former PayPal employees, Yelp is now
 		a publicly traded company (<a href="https://www.nyse.com/quote/XNYS:YELP">NYSE:YELP</a>) with approximately <b>135 monthly visitors</b>.</br></p>
+		<br><b>Frontend of Assignment</b></br>
+		<br><u>Persona</u> - The typical user of Yelp is a person in their 20s or 30s.</br>
+		<br>&#160;&#160;&#160;&#149; There is no male or female bias here.</br>
+		<br>&#160;&#160;&#160;&#149; Yelp users can use the site from mobile or traditional browsers.</br>
+		<br>&#160;&#160;&#160;&#149; The true benefit of Yelp comes from mobile use.</br>
+		<br>&#160;&#160;&#160;&#149; A user can simply open the Yelp app on their smartphone and
+		have Yelp identify nearby businesses.</br>
+		<br>&#160;&#160;&#160;&#149; The typical Yelp user is very <i>opinionated</i>.</br>
+		<br><u>Main Use Cases</u></br>
+
 	</body>
 </html>
