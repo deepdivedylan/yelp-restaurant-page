d5c2fb6d5d3177f5cb728caafdfd471cea3771a7
TrevorRigler <tarigler@gmail.com>
2015-07-19 16:18:17 -0600

    adding thorough comments for html, css, and mysql

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
-<!DOCTYPE html>
+<!DOCTYPE html> <!--message to browser -->
 <html>
-	<head>
+	<head> <!--title of page for browser tab -->
 		<title>Yelp Restaurant Page Data Design Project</title>
-		<link type="image/ico" href="http://s3-media2.fl.yelpcdn.com/assets/2/www/img/118ff475a341/ico/favicon.ico" rel="shortcut icon" />
-		<link rel="stylesheet" type="text/css" href="CSS/styles.css">
+		<link type="image/ico" href="http://s3-media2.fl.yelpcdn.com/assets/2/www/img/118ff475a341/ico/favicon.ico" rel="shortcut icon" /> <!-- stupid favicon -->
+		<link rel="stylesheet" type="text/css" href="CSS/styles.css"> <!-- refers to CSS file -->
 	</head>
 	<body>
-	<div class="container">
+	<div class="container"> <!-- creating a container for header -->
 		<header>
-			<h1>Data Design Project</h1>
+			<h1>Data Design Project</h1> <!--basic page info in header-->
 			<h3>Yelp Restaurant Page</h3>
 			<h4>Trevor Rigler</h4>
 		</header>
-		<img src="images/yelp.jpg" alt="Yelp Logo" height="450" width="800">
+		<img src="images/yelp.jpg" alt="Yelp Logo" height="450" width="800"> <!-- Yelp logo -->
 		<p>Yelp is a website consisting mainly of reviews of local
 			businesses, with an emphasis on restaurants. Founded in 2004 by two former PayPal employees, Yelp is now
 		a publicly traded company (<a href="https://www.nyse.com/quote/XNYS:YELP" target="_blank">NYSE:YELP</a>) with approximately <b>135 monthly visitors</b>.</p>
-	</div>
-			<h2>Frontend of Assignment</h2>
-		<h3>Persona</h3>
-		<ul>
+	</div> <!--some basic info for Yelp and link to their stock info-->
+			<h2>Frontend of Assignment</h2> <!-- dividing the page to reflect the various sections of assignment -->
+		<h3>Persona</h3> <!-- here is our fictional person using Yelp-->
+		<ul> <!-- an unordered list with bullet points-->
 			<li>&#160; Our Persona is Estelle, a 30-year-old associate at an East Coast law firm.</li>
 			<li>&#160; Estelle frequently travels for work to cities throughout the U.S.</li>
 			<li>&#160; Estelle is often pressed for time when traveling and needs information about local businesses quickly.</li>
@@ -28,12 +28,12 @@
 			<li>&#160; Estelle is fairly <i>opinionated</i>.</li>
 		</ul>
 		<h4>Main Use Cases</h4>
-		<br/>&#160;&#160;&#160;<b>I.</b> Search for restaurant by name (static use case)
+		<br/>&#160;&#160;&#160;<b>I.</b> Search for restaurant by name (static use case) <!--first use case, static -->
 		<br/>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Goal: Allow Estelle to find specific restaurant.
 		<br/>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Entities: Estelle, Restaurant
 		<br/>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Assumptions: Estelle has an account. Restaurant has a page.
 		<br/>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Basic Pattern:
-		<ol>
+		<ol> <!-- numerically ordered list of steps of use case -->
 			<li>&#160;&#160;&#160;&#160;Estelle gets a tip from the concierge at her Miami hotel to try a Haitian restaurant called Tap Tap.</li>
 			<li>&#160;&#160;&#160;&#160;Estelle browses to Yelp page for Miami.</li>
 			<li>&#160;&#160;&#160;&#160;Estelle uses Yelp's main search function to find Tap Tap.</li>
@@ -43,12 +43,12 @@
 			<li>&#160;&#160;&#160;&#160;Yelp's server takes Estelle to Yelp's <a href="http://www.yelp.com/biz/tap-tap-restaurant-miami-beach-2" target="_blank">Tap Tap</a> page.</li>
 			<li>&#160;&#160;&#160;&#160;Estelle clicks on pictures, menu, and reviews listed on Tap Tap's Yelp page..</li>
 		</ol>
-			&#160;&#160;&#160;<b>II.</b> Allow Estelle to find other Yelp users she already knows from other sites. (dynamic use case)
+			&#160;&#160;&#160;<b>II.</b> Allow Estelle to find other Yelp users she already knows from other sites. (dynamic use case) <!-- next use case, dynamic -->
 		<br/>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Goal: Allow Estelle to locate other Yelp users based on other website affiliations.
 		<br/>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Entities: Estelle, other Yelp user(s), Third Party Website(s)(ex:Facebook)
 		<br/>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Assumptions: Estelle and the other users have accounts with Yelp <i>and</i> certain third party websites.
 		<br/>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Basic Pattern:
-		<ol>
+		<ol> <!-- numerically ordered list of steps of use case -->
 			<li>&#160;&#160;&#160;&#160;Estelle browses to any Yelp page.</li>
 			<li>&#160;&#160;&#160;&#160;Estelle clicks on Find Friends link near top of page.</li>
 			<li>&#160;&#160;&#160;&#160;Yelp's server offers for Estelle to log in to her Facebook account.</li>
@@ -56,12 +56,12 @@
 			<li>&#160;&#160;&#160;&#160;Yelp's server cross-references existing Yelp users against Estelle's FB friend list.</li>
 			<li>&#160;&#160;&#160;&#160;Estelle selects from list of her FB friends to whom to submit Yelp friend requests.</li>
 		</ol>
-			&#160;&#160;&#160;<b>III.</b> Estelle needs map and directions to Tap Tap. (dynamic use case)
+			&#160;&#160;&#160;<b>III.</b> Estelle needs map and directions to Tap Tap. (dynamic use case) <!-- final use case, dynamic -->
 		<br/>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Goal: Allow Estelle to access map and directions for Tap Tap from third party website(s).
 		<br/>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Entities: Estelle, Restaurant, Third Party Website(s)(ex:Google Maps)
 		<br/>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Assumptions: Estelle has an account with Yelp. Estelle permits Yelp to know user's location. Tap Tap has page with Yelp. Tap Tap is on third party website.
 		<br/>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Basic Pattern:
-		<ol>
+		<ol> <!-- numerically ordered list of steps of use case -->
 		<li>&#160;&#160;&#160;&#160;Estelle browses to Yelp page for Tap Tap.</li>
 		<li>&#160;&#160;&#160;&#160;Estelle clicks on Get Directions link under Tap Tap's name.</li>
 		<li>&#160;&#160;&#160;&#160;Yelp's server searches third party website for Tap Tap's location.</li>
@@ -69,8 +69,8 @@
 		<li>&#160;&#160;&#160;&#160;Estelle clicks on Get Directions button inset on map.</li>
 		<li>&#160;&#160;&#160;&#160;Yelp's server acquires driving directions from third party website and provides same to Estelle in map form.</li>
 		</ol>
-		<h2>Backend of Assignment</h2>
-		<h5>Entities:</h5>
+		<h2>Backend of Assignment</h2> <!--demarcation indicating next part of assignment -->
+		<h5>Entities:</h5> <!-- description of entities leading up to entity-attribute table -->
 			<br/>&#160;&#160;&#160;Profile(s) (strong entity)
 			<br/>&#160;&#160;&#160;Restaurant(s) (strong entity)
 			<br/>&#160;&#160;&#160;Review(s) (weak entity)
@@ -136,10 +136,10 @@
 			</div>
 		</div>
 		<br/>
-			<div class="imgDiagram">
+			<div class="imgDiagram"> <!-- refers to rules set up for this diagram in CSS file -->
 			<figure>
-				<img src="images/erd.jpg" alt="Entity Relationship Diagram"/>
-				<figcaption>Figure 1: Example ERD for the Yelp SQL Database</figcaption>
+				<img src="images/erd.jpg" alt="Entity Relationship Diagram"/> <!-- the ERD -->
+				<figcaption>Figure 1: Example ERD for the Yelp SQL Database</figcaption> <!-- caption for ERD image -->
 			</figure>
 			</div>
 	</body>
--- trevor.ddl
+++ trevor.ddl
@@ -1,34 +1,34 @@
-DROP TABLE IF EXISTS restaurant;
-DROP TABLE IF EXISTS review;
-DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS restaurant; -- dropping this table first as it is created last
+DROP TABLE IF EXISTS review; -- dropping this table next
+DROP TABLE IF EXISTS profile; -- dropping this table last as it is created first
 
- CREATE TABLE profile (
-  profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-  email VARCHAR(128) NOT NULL,
-  password VARCHAR(32),
-  bio VARCHAR(32),
-  INDEX profileId(profileId),
-  UNIQUE(email),
-  PRIMARY KEY(profileId)
+ CREATE TABLE profile ( -- creating table called profile
+  profileId INT UNSIGNED AUTO_INCREMENT NOT NULL, -- unsigned as I don't think the value would ever be negative?
+  email VARCHAR(128) NOT NULL, -- 128 characters for an email address? challenge accepted!
+  password VARCHAR(32), -- 32 characters for password so there are more characters for users to forget
+  bio VARCHAR(32), -- 32 characters for bio info as we don't really want to know too much about users
+  INDEX profileId(profileId), -- creating key for this table
+  UNIQUE(email), -- creating unique attribute
+  PRIMARY KEY(profileId) -- creating primary key for this table
  );
 
-CREATE TABLE review (
- reviewId INT UNSIGNED AUTO_INCREMENT NOT NULL,
- profileId INT UNSIGNED NOT NULL,
- date VARCHAR(32) NOT NULL,
- content VARCHAR(128),
- INDEX reviewId(reviewId),
- FOREIGN KEY(profileId) REFERENCES profile(profileId),
- PRIMARY KEY(reviewId)
+CREATE TABLE review ( -- creating table called review
+ reviewId INT UNSIGNED AUTO_INCREMENT NOT NULL, -- unsigned for same reason as above?
+ profileId INT UNSIGNED NOT NULL, -- link to profile table key
+ date VARCHAR(32) NOT NULL, -- 32 characters for date of review
+ content VARCHAR(128), -- 128 characters for content of review
+ INDEX reviewId(reviewId), -- creating key for this table
+ FOREIGN KEY(profileId) REFERENCES profile(profileId), -- identifying profile table key as foreign key
+ PRIMARY KEY(reviewId) -- creating primary key for this table
 );
 
- CREATE TABLE restaurant (
-  restaurantId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-  reviewId INT UNSIGNED NOT NULL,
-  address VARCHAR(128) NOT NULL,
-  INDEX restaurantId(restaurantId),
-  INDEX(reviewId),
-  FOREIGN KEY(reviewId) REFERENCES review(reviewId),
-  PRIMARY KEY(restaurantId)
+ CREATE TABLE restaurant ( -- creating table called restaurant
+  restaurantId INT UNSIGNED AUTO_INCREMENT NOT NULL, -- unsigned as above
+  reviewId INT UNSIGNED NOT NULL, -- link to review table key
+  address VARCHAR(128) NOT NULL, -- 128 characters for address
+  INDEX restaurantId(restaurantId), -- creating key for this table
+  INDEX(reviewId), -- indexing review table key
+  FOREIGN KEY(reviewId) REFERENCES review(reviewId), -- identifying review table as foreign key
+  PRIMARY KEY(restaurantId) -- creating primary key for this table
  );
 
