95735738f162de2630c7ead2458fb167f33cb82e
TrevorRigler <tarigler@gmail.com>
2015-07-20 17:11:34 -0600

    editing profile.php

--- profile.php
+++ profile.php
@@ -21,4 +21,27 @@ class Profile {
 	 * biographical info for Profile **/
 	private $profileBio;
 }
+	/** this is the accessor method for getting the profile id
+	 * @return int value of profile id **/
+public function getProfileId() {
+	return($this->ProfileId);
+}
+	/** this is the mutator method for ProfileId
+	  * @param int $newProfileId new value of profile id
+	  * @throws InvalidArgumentException if $newProfileId is not an integer or not positive
+	  * @throws RangeException if $newProfileId is not positive
+	  * need to verify that the profile id is valid and positive
+	 **/
+public function setProfileId($newProfileId) {
+	$newProfileId = filter_var($newProfileId, FILTER_VALIDATE_INT);
+	if($newProfileId === false) {
+		throw(new InvalidArgumentException("profile id is not a valid integer"));
+	}
+	if($newProfileId <= 0) {
+		throw(new RangeException("profile id is not positive"));
+	}
+
+	$this->profileId = intval($newProfileId);
+}
+
 ?>
